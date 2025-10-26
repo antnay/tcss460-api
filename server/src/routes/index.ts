4fbb0080d8538c17da8649f165197647bc8318d6
reganlai <reganlai@yahoo.com.hk>
2025-10-26 14:52:22 -0700

    Resolve merge conflicts and fix function names in index.ts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,18 +1,3 @@
-// import { Router } from 'express';
-// import * as c from '../controllers/index'
-// export const router = Router();
-
-
-// router.get('/api-info', c.info);
-// router.get('/health', c.healthCheck);
-
-// router.get('/movies', c.getAllMovies);
-// router.get('/movies/:id', c.getMoviesById);
-
-// router.post('/movies', c.addMovie);
-
-// export default router;
-
 import { Router } from 'express';
 import * as c from '../controllers/index';
 
@@ -22,10 +7,8 @@ export const router = Router();
 router.get('/api-info', c.info);
 router.get('/health', c.healthCheck);
 
-<<<<<<< HEAD
 // GET routes - Retrieve movies
 router.get('/movies', c.getAllMovies);
-=======
 router.post('/login', c.login)
 router.post('/register', c.register)
 router.get('/api-key', c.keyForm)
@@ -38,12 +21,7 @@ router.get('/movies/studio/search', c.getMoviesByStudio);
 router.get('/movies/director/search', c.getMoviesByDirector);
 router.get('/movies/actor/search', c.getMoviesByActor);
 router.get('/movies/collection/search', c.getMoviesByCollection);
-<<<<<<< HEAD
->>>>>>> 1d8b584c764330a01aa22328de2a20c985530cba
 router.get('/movies/:id', c.getMoviesById);
-=======
-router.get('/movies/:id', c.getMovieById);
->>>>>>> d1473a3eb23e505548d431124f84e2ca74c2182b
 
 // POST routes - Add movies
 router.post('/movies', c.addMovie);
