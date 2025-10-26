1d8b584c764330a01aa22328de2a20c985530cba
Anthony <antnay@uw.edu>
2025-10-26 14:12:36 -0700

    feat: maybe api key idk what im doing anymore

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -8,9 +8,11 @@ router.get('/health', c.healthCheck);
 
 router.post('/login', c.login)
 router.post('/register', c.register)
+router.get('/api-key', c.keyForm)
+router.post('/api-key', c.generateKey)
 
 router.get('/movies', c.getAllMovies);
-router.get('/movies/search/financials', c.getMoviesByFinancials);
+router.get('/movies/search/financial', c.getMoviesByFinancial);
 router.get('/movies/search/multi', c.getMoviesByMultiFilter);
 router.get('/movies/studio/search', c.getMoviesByStudio);
 router.get('/movies/director/search', c.getMoviesByDirector);
